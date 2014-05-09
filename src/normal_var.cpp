b004937043b798126c4eefdea71c157104fdc513
Michael Lindon <michael.s.lindon@gmail.com>
2014-05-09 18:44:47 -0400

    Randomized probabilities

--- src/normal_var.cpp
+++ src/normal_var.cpp
@@ -10,7 +10,7 @@ List normal_var(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Numeric
 	//Define Variables//
 	int p=rxo.ncol();
 	int no=rxo.nrow();
-	int na=rxa.ncol();
+	int na=rxa.nrow();
 	int t=1;
 	double a=(no-1)/2;
 	double b;
@@ -31,10 +31,10 @@ List normal_var(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Numeric
 	Mat<double> Ina=eye(na,na);
 	Mat<double> P1(no,no);
 	Mat<double> Px(no,no);
-	Mat<double> mu_trace(na,10000,fill::zeros);
-	Mat<double> H_trace(na*na,10000,fill::zeros);
-	Mat<double> prob_trace(p,10000,fill::zeros);
-	Col<double> b_trace(10000);
+	Mat<double> mu_trace(na,1000,fill::zeros);
+	//Mat<double> H_trace(na*na,1000,fill::zeros);
+	Mat<double> prob_trace(p,1000,fill::zeros);
+	Col<double> b_trace(1000);
 	Col<double> one(no,fill::ones);
 	Col<double> mu(na,fill::zeros);
 	Col<double> d(p);
@@ -46,7 +46,6 @@ List normal_var(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Numeric
 	Col<double> ldl(p);
 	Col<double> dli(p);
 
-
 	//Copy RData Into Matrix Classes//
 	arma::mat xo(rxo.begin(), no, p, false);
 	arma::mat xa(rxa.begin(), na, p, false);
@@ -54,37 +53,33 @@ List normal_var(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Numeric
 	arma::colvec lam(rlam.begin(),rlam.size(), false);
 	arma::colvec priorprob(rpriorprob.begin(),rpriorprob.size(), false);
 
-
 	//Create Matrices//
+	xoyo=xo.t()*yo;
 	xoxo=xo.t()*xo;
 	xaxa=xa.t()*xa;
 	D=xaxa+xoxo;
 	d=D.diag();
-
-
-	//Initialize Parameters at MLE//
+	Lam=diagmat(lam);
+	priorodds=priorprob/(1-priorprob);
+	ldl=sqrt(lam/(d+lam));
+	dli=1/(d+lam);
+	xcxcLami=diagmat(1/(d+lam));
 	P1=one*(one.t()*one).i()*one.t();
 	Px=xo*(xoxo).i()*xo.t();
-	phi=(no-1)/dot(yo,((Ino-P1-Px)*yo));
+	varyo=dot(yo,(Ino-P1)*yo);
 
-
-	//Single Instance Computations//
-	Lam=diagmat(lam);
-	for (int i = 0; i < p; ++i)
-	{
-		priorodds(i)=priorprob(i)/(1-priorprob(i));
-		ldl(i)=sqrt(lam(i)/(d(i)+lam(i)));
-		dli(i)=1/(d(i)+lam(i));
-	}
-
+	//Randomize Initial Probabilities//
+	for (int i = 0; i < p; ++i) prob(i)=R::runif(0,1);
+	P.diag()=prob;
+	Pi.diag()=1/prob;
 
 	//Run Variational//
-	xoyo=xo.t()*yo;
-	xcxcLami=diagmat(1/(d+lam));
-	varyo=dot(yo,(Ino-P1)*yo);
+	prob_trace.col(0)=prob;
+	mu_trace.col(0)=mu;
+	//H_trace.col(0)=vectorise(H);
+	b_trace(0)=b;
 	do{
-
-
+    cout << t << endl;
 		//Phi Step//
 		Q=(D+Lam)*Pi;
 		b=0.5*(varyo-dot(xoyo,solve(Q-xaxa,xoyo)));
@@ -93,8 +88,6 @@ List normal_var(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Numeric
 		//Ya Step//
 		mu=solve(Ina-xa*P*xcxcLami*xa.t(),xa*P*xcxcLami*xoyo);
 		H=Ina-xa*P*xcxcLami*xa.t();
-
-
 
 		//Probability Step//
 		for (int i = 0; i < p; i++)
@@ -106,20 +99,20 @@ List normal_var(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Numeric
 		P.diag()=prob;
 		Pi.diag()=1/prob;
 
-
 		//Store Values//
 		prob_trace.col(t)=prob;
 		mu_trace.col(t)=mu;
-		H_trace.col(t)=vectorise(H);
+	//	H_trace.col(t)=vectorise(H);
 		b_trace(t)=b;
 
 		delta=dot(prob_trace.col(t)-prob_trace.col(t-1),prob_trace.col(t)-prob_trace.col(t-1));
+    cout << delta << endl;
 		t=t+1;
 	}while (delta>0.001);
 
 	prob_trace.resize(p,t);
 	mu_trace.resize(p,t);
-	H_trace.resize(na*na,t);
+//	H_trace.resize(na*na,t);
 	b_trace.resize(t);
 
 	return Rcpp::List::create(
@@ -127,7 +120,7 @@ List normal_var(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Numeric
 			Rcpp::Named("prob_trace") = prob_trace,
 			Rcpp::Named("mu_trace") = mu_trace,
 			Rcpp::Named("mu") = mu,
-			Rcpp::Named("H_trace") = H_trace,
+		//	Rcpp::Named("H_trace") = H_trace,
 			Rcpp::Named("H") = H,
 			Rcpp::Named("b_trace") = b_trace,
 			Rcpp::Named("b") = b
