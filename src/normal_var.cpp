161528eb7ba6c58ee5cbcb2d91af6d736e8c1aaf
Michael Lindon <michael.s.lindon@gmail.com>
2014-05-10 12:38:16 -0400

    Tidied some stuff up

--- src/normal_var.cpp
+++ src/normal_var.cpp
@@ -15,24 +15,26 @@ List normal_var(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Numeric
 	double a=(no-1)/2;
 	double b;
 	double phi;
-	double varyo;
 	double delta;
+	Mat<double> Aooa(no,no);
+	Mat<double> A(no+na,no+na);
+	Mat<double> Aoo(no,no);
+	Mat<double> Aaa(na,na);
+	Mat<double> Aoa(no,na);
+	Mat<double> Aao(na,no);
+	Mat<double> Aaai(na,na);
 	Mat<double> xaxa(p,p);
 	Mat<double> xoxo(p,p);
-	Mat<double> xoxog(p,p);
-	Mat<double> D(p,p);
-	Mat<double> Q(p,p);
-	Mat<double> Lam(p,p);
 	Mat<double> xcxcLami(p,p);
-	Mat<double> H(na,na,fill::eye);
+	Mat<double> D(p,p);
+	Mat<double> Lam(p,p);
+	Mat<double> xc(no+na,p);
 	Mat<double> Ino=eye(no,no);
-	Mat<double> P=eye(p,p);
-	Mat<double> Pi=eye(p,p);
 	Mat<double> Ina=eye(na,na);
+	Mat<double> Inc=eye(no+na,no+na);
+	Mat<double> P=eye(p,p);
 	Mat<double> P1(no,no);
-	Mat<double> Px(no,no);
 	Mat<double> mu_trace(na,1000,fill::zeros);
-	//Mat<double> H_trace(na*na,1000,fill::zeros);
 	Mat<double> prob_trace(p,1000,fill::zeros);
 	Col<double> b_trace(1000);
 	Col<double> one(no,fill::ones);
@@ -54,65 +56,62 @@ List normal_var(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Numeric
 	arma::colvec priorprob(rpriorprob.begin(),rpriorprob.size(), false);
 
 	//Create Matrices//
+	xc=join_cols(xo,xa);
 	xoyo=xo.t()*yo;
 	xoxo=xo.t()*xo;
 	xaxa=xa.t()*xa;
 	D=xaxa+xoxo;
 	d=D.diag();
+	xcxcLami=diagmat(1/(d+lam));
 	Lam=diagmat(lam);
 	priorodds=priorprob/(1-priorprob);
 	ldl=sqrt(lam/(d+lam));
 	dli=1/(d+lam);
-	xcxcLami=diagmat(1/(d+lam));
 	P1=one*(one.t()*one).i()*one.t();
-	Px=xo*(xoxo).i()*xo.t();
-	varyo=dot(yo,(Ino-P1)*yo);
 
 	//Randomize Initial Probabilities//
 	for (int i = 0; i < p; ++i) prob(i)=R::runif(0,1);
 	P.diag()=prob;
-	Pi.diag()=1/prob;
 
 	//Run Variational//
 	prob_trace.col(0)=prob;
 	mu_trace.col(0)=mu;
-	//H_trace.col(0)=vectorise(H);
 	b_trace(0)=b;
 	do{
-    cout << t << endl;
-		//Phi Step//
-		Q=(D+Lam)*Pi;
-		b=0.5*(varyo-dot(xoyo,solve(Q-xaxa,xoyo)));
+		//Phi Maximization Step//
+		A=Inc-xc*P*xcxcLami*xc.t();
+		Aoo=A.submat(0,0,no-1,no-1)-P1;
+		Aaa=A.submat(no,no,no+na-1,no+na-1);
+		Aoa=A.submat(0,no,no-1,no+na-1);
+		Aao=A.submat(no,0,no+na-1,no-1);
+		Aaai=inv_sympd(Aaa);
+		Aooa=Aoo-Aoa*Aaai*Aao;
+		b=0.5*(dot(yo,Aooa*yo));
 		phi=((double)a)/b;
 
-		//Ya Step//
-		mu=solve(Ina-xa*P*xcxcLami*xa.t(),xa*P*xcxcLami*xoyo);
-		H=Ina-xa*P*xcxcLami*xa.t();
+		//Ya Maximization Step//
+		mu=-Aaai*Aao*yo;
 
 		//Probability Step//
 		for (int i = 0; i < p; i++)
 		{
 			Bols(i)=(1/d(i))*(xoyo(i)+dot(xa.col(i),mu));
-			odds(i)=priorodds(i)*ldl(i)*trunc_exp(0.5*phi*dli(i)*(d(i)*d(i)*Bols(i)*Bols(i)+0.5*dli(i)*dot(xa.col(i),solve(H,xa.col(i))) ));
+			odds(i)=priorodds(i)*ldl(i)*trunc_exp(0.5*phi*dli(i)*d(i)*d(i)*Bols(i)*Bols(i)+0.5*dli(i)*dot(xa.col(i),Aaai*xa.col(i)) );
 			prob(i)=odds(i)/(1+odds(i));
 		}
 		P.diag()=prob;
-		Pi.diag()=1/prob;
 
 		//Store Values//
 		prob_trace.col(t)=prob;
 		mu_trace.col(t)=mu;
-	//	H_trace.col(t)=vectorise(H);
 		b_trace(t)=b;
 
 		delta=dot(prob_trace.col(t)-prob_trace.col(t-1),prob_trace.col(t)-prob_trace.col(t-1));
-    cout << delta << endl;
 		t=t+1;
 	}while (delta>0.001);
 
 	prob_trace.resize(p,t);
 	mu_trace.resize(p,t);
-//	H_trace.resize(na*na,t);
 	b_trace.resize(t);
 
 	return Rcpp::List::create(
@@ -120,8 +119,7 @@ List normal_var(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Numeric
 			Rcpp::Named("prob_trace") = prob_trace,
 			Rcpp::Named("mu_trace") = mu_trace,
 			Rcpp::Named("mu") = mu,
-		//	Rcpp::Named("H_trace") = H_trace,
-			Rcpp::Named("H") = H,
+			Rcpp::Named("E") = Aaai,
 			Rcpp::Named("b_trace") = b_trace,
 			Rcpp::Named("b") = b
 			) ;
