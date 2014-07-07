8fc53083644398c65c2bd2cbb429cc66d2113d55
Michael Lindon <michael.s.lindon@gmail.com>
2014-07-06 23:57:27 -0400

    cleaned up and commented

--- src/normal_em.cpp
+++ src/normal_em.cpp
@@ -10,12 +10,13 @@ double log_posterior_density(int no,const Col<double>& lam, const Col<uword>& ga
 // [[Rcpp::export]]
 List normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericVector rd, NumericVector rlam, NumericVector rpriorprob){
 
-
+	//Define Variables//
 	int p=rxo.ncol();
 	int no=rxo.nrow();
 	int na=rxa.nrow();
+
+	//Create Data//
 	arma::mat xo(rxo.begin(), no, p, false);
-	//arma::mat xoxo(rxoxo.begin(), p, p, false);
 	arma::mat xa(rxa.begin(), na, p, false);
 	arma::colvec d(rd.begin(),rd.size(), false);
 	arma::colvec lam(rlam.begin(),rlam.size(), false);
@@ -23,7 +24,7 @@ List normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericV
 	arma::colvec yo(ryo.begin(), ryo.size(), false);
 	yo-=mean(yo);
 
-
+	//Pre-Processing//
 	Mat<double> Ino=eye(no,no);
 	Mat<double> Ina=eye(na,na);
 	Col<double> xoyo=xo.t()*yo;
@@ -34,24 +35,28 @@ List normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericV
 	Col<double> mu_ya=xa*B;
 	Mat<double> xat=xa.t();
 	Col<double> xamu_ya=xat*mu_ya;
-
 	Mat<double> Lam=diagmat(lam);
 	Col<double> prob=priorprob;
 	Col<double> priorodds=priorprob/(1-priorprob);
 	Col<double> odds=priorodds;
 	Col<double> ldl=sqrt(lam/(d+lam));
 	Col<double> dli=1/(d+lam);
-
-	Col<uword> gamma(p,fill::zeros);
-	Col<uword> inc_indices(p,fill::ones);
-	//Randomize Initial Gammas//
-	for (int i = 0; i < p; ++i) if(R::runif(0,1)>0.5) gamma(i)=1;
+	double a;
+	double b;
+	double deltaP;
+	double deltaB;
+	double phi;
 
 	//Create Submatrices
-	Mat<double> xog;
-	Mat<double> xag;
+	Col<uword> gamma(p,fill::zeros);
+	Col<uword> inc_indices(p,fill::ones);
+	Mat<double> xog(no,p);
+	Mat<double> xag(na,p);
 	Mat<double> Lamg(p,p);
-	Col<double> Bg;
+	Col<double> Bg(p,fill::zeros);
+
+	//Randomize Initial Gammas//
+	for (int i = 0; i < p; ++i) if(R::runif(0,1)>0.5) gamma(i)=1;
 
 	//Create Trace Matrices
 	Mat<double> prob_trace(p,10000);
@@ -60,12 +65,6 @@ List normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericV
 	Col<double> lpd_trace(10000);
 	Col<double> a_trace(10000);
 	Col<double> b_trace(10000);
-
-	double a;
-	double b;
-	double deltaP;
-	double deltaB;
-	double phi;
 
 	//Run EM Algorithm//
 	gamma_trace.col(0)=gamma;
@@ -80,19 +79,17 @@ List normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericV
 		xag=xa.cols(inc_indices);
 		xog=xo.cols(inc_indices);
 
-
-		//E-Step//
-		//Phi//
+		//E-Step Phi//
 		b=(double)0.5*dot(yo-xog*Bg,yo-xog*Bg)+0.5*dot(Bg,Lamg*Bg);
 		a=(double)0.5*(no+sum(gamma)-1);
 		phi=a/b;
-		//	lpd_trace(t-1)= log_posterior_density( no,lam, gamma, priorodds, a,  b, p);
+		lpd_trace(t-1)= log_posterior_density( no,lam, gamma, priorodds, a,  b, p);
 
-		//Ya//
+		//E-Step Ya//
 		mu_ya=xag*Bg;
 
+		//M-Step (Beta,Gamma)//
 		xamu_ya=xat*mu_ya;
-		//Gamma//
 		for (int i = 0; i < p; ++i)
 		{
 			Bols(i)=(1/d(i))*(xoyo(i)+xamu_ya(i));
@@ -100,7 +97,6 @@ List normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericV
 			prob(i)=odds(i)/(1+odds(i));
 			//if(prob(i)!=prob(i)) prob(i)=1;	 //Catch NaN
 
-			//M-Step//
 			//Choose Median Probability Model//
 			if(prob(i)*sqrt( ((d(i)+lam(i))*(phi))/(2*M_PI) )>(1-prob(i))){
 				gamma(i)=1;
@@ -110,8 +106,6 @@ List normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericV
 				B(i)=0;
 			}
 		}
-
-
 
 		//Store Values//
 		a_trace(t)=a;
@@ -123,17 +117,15 @@ List normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericV
 		deltaP=dot(prob_trace.col(t)-prob_trace.col(t-1),prob_trace.col(t)-prob_trace.col(t-1));
 		deltaB=dot(B_trace.col(t)-B_trace.col(t-1),B_trace.col(t)-B_trace.col(t-1));
 		t=t+1;
-	} while(deltaP>0.0001 || deltaB>0.0001);
+	} while(deltaP>0.0000001 || deltaB>0.000000001);
 
+	//Report Runtime//
 	auto end = std::chrono::steady_clock::now();
 	std::chrono::duration<double> elapsed=end-start;
-
 	std::cout <<  elapsed.count() << " sec - Total Runtime" << std::endl;
 	std::cout <<  elapsed.count()/(t-1) << " sec - Per Iteration (avg)" << std::endl;
 
-
-
-
+	//Resize Trace Matrices//
 	gamma_trace.resize(p,t);
 	prob_trace.resize(p,t);
 	B_trace.resize(p,t);
@@ -141,8 +133,8 @@ List normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericV
 	a_trace.resize(t);
 	b_trace.resize(t);
 
+	//Report Top Model//
 	Col<uword> top_model=find(gamma)+1;
-
 	cout << "Top Model Predictors" << endl;
 	cout << top_model << endl;
 
@@ -158,7 +150,7 @@ List normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericV
 			Rcpp::Named("B_trace") = B_trace,
 			Rcpp::Named("gamma") = gamma,
 			Rcpp::Named("gamma_trace") = gamma_trace,
-			Rcpp::Named("log_posterior_density_trace") = lpd_trace
+			Rcpp::Named("lpd_trace") = lpd_trace
 			) ;
 
 }
