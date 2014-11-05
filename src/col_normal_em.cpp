6b717eb62705203d240e79a8f45bea219384d8cb
Michael Lindon <michael.s.lindon@gmail.com>
2014-11-04 19:52:46 -0500

    Doing everything in terms of xaxa now

--- src/col_normal_em.cpp
+++ src/col_normal_em.cpp
@@ -7,18 +7,18 @@ using namespace arma;
 double log_posterior_density_colcn(const Col<double>& yo, const Mat<double>& Lam, const Mat<double>& xo, const Mat<double>& xoxo, const Col<uword>& gamma,const Col<double>& priorodds);
 
 // [[Rcpp::export]]
-List col_normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, NumericVector rd, NumericVector rlam, NumericVector rpriorprob, SEXP rselection){
+List col_normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxaxa, NumericVector rd, NumericVector rlam, NumericVector rpriorprob, SEXP rselection){
 
 	//Define Variables//
 	int p=rxo.ncol();
 	int no=rxo.nrow();
-	int na=rxa.nrow();
+	int na=rxaxa.nrow();
 	int selection=Rcpp::as<int >(rselection);
 
 
 	//Create Data//
 	arma::mat xo(rxo.begin(), no, p, false);
-	arma::mat xa(rxa.begin(), na, p, false);
+	arma::mat xaxa(rxaxa.begin(), na, p, false);
 	arma::colvec d(rd.begin(),rd.size(), false);
 	arma::colvec lam(rlam.begin(),rlam.size(), false);
 	arma::colvec priorprob(rpriorprob.begin(),rpriorprob.size(), false);
@@ -26,13 +26,13 @@ List col_normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Nume
 	yo-=mean(yo);
 
 	//Pre-Processing//
+	Mat<double> foo(p,p);
 	Col<double> xoyo=xo.t()*yo;
 	Mat<double> xoxo=xo.t()*xo;
 	Col<double> B(p);
 	Col<double> Bols=B;
 	Col<double> mu_ya(na);
-	Mat<double> xat=xa.t();
-	Col<double> xamu_ya(p);
+	Col<double> xaya(p);
 	Mat<double> Lam=diagmat(lam);
 	Col<double> prob=priorprob;
 	Col<double> priorodds=priorprob/(1-priorprob);
@@ -51,10 +51,9 @@ List col_normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Nume
 	Col<uword> inc_indices(p);
 	Col<uword> greedy_sort(p);
 	Mat<double> xog(no,p);
-	Mat<double> xoxog(p,p);
-	Mat<double> xag(na,p);
+	Mat<double> xogxog(p,p);
+	Mat<double> xaxag(na,p);
 	Mat<double> Lamg(p,p);
-	Mat<double> E(na,na);
 
 	//Create Matrices//
 	Mat<double> prob_trace(p,1000,fill::zeros);
@@ -88,29 +87,26 @@ List col_normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Nume
 		//Form Submatrices
 		inc_indices=find(gamma);
 		Lamg=Lam.submat(inc_indices,inc_indices);
-		xag=xa.cols(inc_indices);
 		xog=xo.cols(inc_indices);
-		xoxog=xoxo.submat(inc_indices,inc_indices);
+		xaxag=xaxa.cols(inc_indices);
+		xogxog=xoxo.submat(inc_indices,inc_indices);
 
-		B=solve(xoxog+Lamg,xog.t()*yo);
+		foo=(xogxog+Lamg).i();
+		B=foo*xog.t()*yo;
 
 		//E-Step//
 		//Phi//
-		b=0.5*(yoyo - dot(B,(xoxog+Lamg)*B));
+		b=0.5*(yoyo - dot(B,(xogxog+Lamg)*B));
 		lpd_trace(t-1)=log_posterior_density_colcn( yo, Lam, xo, xoxo, gamma, priorodds);
 
 		//Ya//
-		mu_ya=xag*B;
-		//	E=Ina+xag*(xoxog+Lamg).i()*xag.t(); Identity Matrix requires a lot of memory to store
-		E=xag*(xoxog+Lamg).i()*xag.t();
-		for(int i=0; i<p; ++i) E(i,i)+=1;
+		xaya=xaxag*B;
 
-		xamu_ya=xat*mu_ya;
 		//Gamma//
 		for (int i = 0; i < p; ++i)
 		{
-			Bols(i)=(1/d(i))*(xoyo(i)+xamu_ya(i));
-			odds(i)=priorodds(i)*ldl(i)*trunc_exp(0.5*(a/b)*dli(i)*d(i)*d(i)*Bols(i)*Bols(i)+0.5*dli(i)*dot(xa.col(i),E*xa.col(i)));
+			Bols(i)=(1/d(i))*(xoyo(i)+xaya(i));
+			odds(i)=priorodds(i)*ldl(i)*trunc_exp(0.5*(a/b)*dli(i)*d(i)*d(i)*Bols(i)*Bols(i)+0.5*dli(i)*xaxa(i,i)+0.5*dli(i)*dot(xaxag.row(i),foo*(xaxag.row(i).t())));
 			prob(i)=odds(i)/(1+odds(i));
 			//if(prob(i)!=prob(i)) prob(i)=1;	 //Catch NaN
 
@@ -121,10 +117,8 @@ List col_normal_em(NumericVector ryo, NumericMatrix rxo, NumericMatrix rxa, Nume
 			}else{
 				gamma(i)=0;
 			}
-
-
-
 		}
+
 		if(selection==3){
 			if(sum(abs(gamma-gamma_trace.col(t-1)))==0){
 				inc_indices=find(prob<=0.5);
@@ -174,8 +168,8 @@ double log_posterior_density_colcn(const Col<double>& yo, const Mat<double>& Lam
 	Col<uword> inc_indices=find(gamma);
 	Mat<double> Lamg=Lam.submat(inc_indices,inc_indices);
 	Mat<double> xog=xo.cols(inc_indices);
-	Mat<double> xoxog=xoxo.submat(inc_indices,inc_indices);
+	Mat<double> xogxog=xoxo.submat(inc_indices,inc_indices);
 
-	return(0.5*log(det(Lamg))-0.5*log(det(Lamg+xoxog))-0.5*(no-1)*log(0.5*(dot(yo,yo) - dot(xog.t()*yo,solve(xoxog+Lamg,xog.t()*yo))))+sum(gamma%log(priorodds)));
+	return(0.5*log(det(Lamg))-0.5*log(det(Lamg+xogxog))-0.5*(no-1)*log(0.5*(dot(yo,yo) - dot(xog.t()*yo,solve(xogxog+Lamg,xog.t()*yo))))+sum(gamma%log(priorodds)));
 
 }
