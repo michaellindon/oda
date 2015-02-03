493016e27d44cf0ed2b21b97cd6bfcc4b889b3c4
Michael Lindon <michael.s.lindon@gmail.com>
2015-02-03 00:18:21 -0500

    removed ldl and dli

--- src/fixed_probabilities.cpp
+++ src/fixed_probabilities.cpp
@@ -1,10 +1,10 @@
 #include "oda.h"
 
-void fixed_probabilities(std::vector<double> &prob, std::vector<double> &odds, std::vector<double> &Bols, const std::vector<double> &d, const std::vector<double> &xoyo, const std::vector<double> &xaya, const std::vector<double> &priorodds, const std::vector<double> &ldl, const std::vector<double> &dli, double phi){
+void fixed_probabilities(std::vector<double> &prob, std::vector<double> &odds, std::vector<double> &Bols, const std::vector<double> &d, const std::vector<double> &xoyo, const std::vector<double> &xaya, const std::vector<double> &priorprob, const std::vector<double> &lam, const double phi){
 	for(size_t i=0; i!=prob.size(); ++i)
 	{
 		Bols[i]=(1/d[i])*(xoyo[i]+xaya[i]);
-		odds[i]=priorodds[i]*ldl[i]*exp(0.5*phi*dli[i]*d[i]*d[i]*Bols[i]*Bols[i]);
+		odds[i]=(priorprob[i]/(1-priorprob[i]))*sqrt(lam[i]/(lam[i]+d[i]))*exp(0.5*phi*((d[i]*d[i])/(d[i]+lam[i]))*Bols[i]*Bols[i]);
 		prob[i]=odds[i]/(1+odds[i]);
 		if(prob[i]!=prob[i]) prob[i]=1;	 //Catch NaN exponential
 	}
