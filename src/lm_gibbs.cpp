68fa24370c6ed2456096c972d1b8e06fa688e6a3
Michael Lindon <michael.s.lindon@gmail.com>
2015-02-07 11:41:07 -0500

    changed gamma_mcmc arguement type

--- src/lm_gibbs.cpp
+++ src/lm_gibbs.cpp
@@ -1,6 +1,6 @@
 #include "oda.h"
 
-extern "C" void lm_gibbs(double * ryo, double * rxo, double * rxa, double * rxoxo, double * rd, double * rlam, int * rmodelprior, double * rpriorprob, double * rbeta1, double * rbeta2, int * rburnin, int * rniter, int * rscalemixture, double * ralpha, int * rcollapsed, int * rno, int * rna, int * rp, double * B_mcmc, double * prob_mcmc, double * gamma_mcmc, double * phi_mcmc, double * B_rb, double * prob_rb)
+extern "C" void lm_gibbs(double * ryo, double * rxo, double * rxa, double * rxoxo, double * rd, double * rlam, int * rmodelprior, double * rpriorprob, double * rbeta1, double * rbeta2, int * rburnin, int * rniter, int * rscalemixture, double * ralpha, int * rcollapsed, int * rno, int * rna, int * rp, double * B_mcmc, double * prob_mcmc, int * gamma_mcmc, double * phi_mcmc, double * B_rb, double * prob_rb)
 {
 	//MCMC Variables//
 	int burnin=*rburnin;
