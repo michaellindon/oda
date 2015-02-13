6ca086fa1445cb9bfc1b31d48a8c16df7060ce39
Michael Lindon <michael.s.lindon@gmail.com>
2015-02-13 14:47:01 -0500

    Cannot avoid recopy because dpotrf destroys upper right

--- src/submatrices_collapsed.cpp
+++ src/submatrices_collapsed.cpp
@@ -6,7 +6,6 @@ void submatrices_collapsed(const bool gamma_diff, std::vector<double> &mu, std::
 	{
 		xag.resize(0);
 		xogyo.resize(0);
-		xogxog_Lamg.resize(0);
 		for(int i=0; i<p; ++i)
 		{
 			if(gamma[i]==1)
@@ -14,27 +13,26 @@ void submatrices_collapsed(const bool gamma_diff, std::vector<double> &mu, std::
 				xogyo.push_back(xoyo[i]);
 				for(int j=0; j<=i; ++j) xag.push_back(xa[j+(i+1)*i/2]); 
 				for(int j=i+1; j!=p; ++j) xag.push_back(0);
-				for(int j=0; j<p; ++j) if(gamma[j]==1) xogxog_Lamg.push_back(xoxo[i*p+j]);
 			}
 		}
 	}
 
-//	for(std::vector<double>::iterator it=xag.begin(); it!=xag.end(); ++it) std::cout << *it << std::endl;
+	//	for(std::vector<double>::iterator it=xag.begin(); it!=xag.end(); ++it) std::cout << *it << std::endl;
 
+	xogxog_Lamg.resize(0);
 	lamg.resize(0);
 	Bg.resize(0);
-	int j=0;
 	for(int i=0; i<p; ++i)
 	{
 		if(gamma[i]==1)
 		{
 			Bg.push_back(xoyo[i]);
 			lamg.push_back(lam[i]);
-			xogxog_Lamg[j*p_gamma+j]=xoxo[i*p+i]+lam[i];
-			++j;
+			for(int j=0; j<p; ++j) if(gamma[j]==1) xogxog_Lamg.push_back(xoxo[i*p+j]);
 		}
 	}
 
+	for(int i=0; i<p_gamma; ++i) xogxog_Lamg[i*p_gamma+i]+=lamg[i];
 	dpotrf_( &uplo, &p_gamma, &*xogxog_Lamg.begin(), &p_gamma, &info); //xerbla handles info error
 	dpotrs_( &uplo, &p_gamma, &nrhs, &*xogxog_Lamg.begin(), &p_gamma, &*Bg.begin(), &p_gamma, &info);
 	b=0.5*(yoyo-ddot_(&p_gamma, &*xogyo.begin(), &inc, &*Bg.begin(), &inc));
