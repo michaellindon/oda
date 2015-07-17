4c93c0c999b9f58ff60654e1144e00fde2275ed0
Michael Lindon <michael.s.lindon@gmail.com>
2015-07-17 02:22:35 +0100

    working

--- src/em.cpp
+++ src/em.cpp
@@ -42,7 +42,29 @@ extern "C" void em(double * ryo, double * rxo,  double * rlam, int * rmodelprior
 	//Construct Xa//
 	std::vector<double> xa(p*(p+1)/2); //Triangular Packed Storage
 	std::vector<double> d(p);
-	chol_xa(xa,xoxo,d,p);
+	std::vector<double> xaxa(xoxo);
+	std::vector<double> xaxag;
+	xaxag.reserve(p*p);
+	char eigenvec='N';
+	char eigenrange='I';
+	double eigenprec=0.001;
+	int no_eigenval=0;
+	double eigenvalues[1];
+	double * vl=NULL;
+	double * vu=NULL;
+	double * eigenvectors=NULL;
+	int iwork[5*p];
+	int ifail[p];
+	int lwork=8*p;
+	double work[lwork];
+	dsyevx_(&eigenvec,&eigenrange,&uplo,&p, &*xaxa.begin(), &p,vl,vu,&p,&p,&eigenprec,&no_eigenval,&eigenvalues[0], eigenvectors, &p, &work[0], &lwork, &iwork[0], &ifail[0], &info);
+	std::copy(xoxo.begin(),xoxo.end(),xaxa.begin());
+	std::transform(xaxa.begin(), xaxa.end(), xaxa.begin(), bind2nd(std::multiplies<double>(),-1.0));	
+	for(int i=0; i!=p; ++i)
+	{
+		d[i]=eigenvalues[0]*0.5;
+		xaxa[i*p+i]+=d[i];
+	}
 
 
 	//Reserve Memory for Submatrices//
@@ -109,63 +131,64 @@ extern "C" void em(double * ryo, double * rxo,  double * rlam, int * rmodelprior
 			stackgamma.pop();
 			phi=stackphi.top();
 			stackphi.pop();
-		p_gamma=std::accumulate(gamma.begin(),gamma.end(),0);
+			p_gamma=std::accumulate(gamma.begin(),gamma.end(),0);
 		}else{
 			std::cout << "Refresh" << std::endl;
-		for(int i=0; i<p; ++i){
-			if(Rf_runif(0,1)<0.5){
-				gamma[i]=1; //Note gamma is initialized at zero
-				B[i]=Rf_rnorm(0,1);
+			for(int i=0; i<p; ++i){
+				if(Rf_runif(0,1)<0.5){
+					gamma[i]=1; //Note gamma is initialized at zero
+					B[i]=Rf_rnorm(0,1);
+				}
 			}
-		}
 		} 
-	do
-	{
-		if(p_gamma) submatrices_uncollapsed(gamma_diff,B,xog,xag,lamg,Bg,gamma,lam,xo,xa,p_gamma,p,no,na);
-		if(p_gamma){
-			a=0.5*(no+p_gamma-3);
-			std::vector<double> residual=yo;
-			dgemv_(&transN , &no, &p_gamma, &nunity, &*xog.begin(), &no, &*Bg.begin(), &inc, &inputscale1, &*residual.begin(), &inc);
-			b=0.5*ddot_(&no, &*residual.begin(), &inc, &*residual.begin(), &inc);
-			for(size_t i=0; i!=Bg.size(); ++i) b+=0.5*Bg[i]*Bg[i]*lamg[i]; 
-		}else{
-			a=0.5*(no-3);
-			b=0.5*yoyo;
-		}
-		lpd_trace[t-1]=log_posterior_density(gamma,priorprob,phi,a,b);
-		phi=a/b;
-
-		if(p_gamma!=0){
-			dgemv_(&transN , &na, &p_gamma, &unity, &*xag.begin(), &na, &*Bg.begin(), &inc, &inputscale0, &*xaya.begin(), &inc);
-			//dtrmv_(&uplo, &transT, &unit_tri, &na, &*xa.begin(), &na, &*xaya.begin(), &inc);
-			dtpmv_(&uplo, &transT, &unit_tri, &na, &*xa.begin(), &*xaya.begin(), &inc);
-		}else{
-			for(size_t i=0; i!=xaya.size(); ++i) xaya[i]=0;
-			//dtrmv_(&uplo, &transT, &unit_tri, &na, &*xa.begin(), &na, &*xaya.begin(), &inc);
-		}
-
-		//Compute Probabilities//
-		if(modelprior==1)
+		do
 		{
-			bernoulli_probabilities(prob,odds,Bols,d,xoyo,xaya,priorprob,lam,phi);
-		}else if(modelprior==2)
-		{
-			betabinomial_probabilities(prob,odds,Bols,d,xoyo,xaya,theta,lam,phi);
-		}else
-		{
-			uniform_probabilities(prob,odds,Bols,d,xoyo,xaya,lam,phi);
-		}
-
-		for (int i = 0; i < p; ++i)
-		{
-			if(prob[i]*sqrt(phi*(lam[i]+d[i])/(2*M_PI))>(1-prob[i])){
-				gammaProp[i]=1;
+			if(p_gamma) submatrices_uncollapsed(gamma_diff,B,xog,xag,lamg,Bg,gamma,lam,xo,xa,p_gamma,p,no,na);
+			if(p_gamma){
+				a=0.5*(no+p_gamma-3);
+				std::vector<double> residual=yo;
+				dgemv_(&transN , &no, &p_gamma, &nunity, &*xog.begin(), &no, &*Bg.begin(), &inc, &inputscale1, &*residual.begin(), &inc);
+				b=0.5*ddot_(&no, &*residual.begin(), &inc, &*residual.begin(), &inc);
+				for(size_t i=0; i!=Bg.size(); ++i) b+=0.5*Bg[i]*Bg[i]*lamg[i]; 
 			}else{
-				gammaProp[i]=0;
+				a=0.5*(no-3);
+				b=0.5*yoyo;
 			}
-		}
+			lpd_trace[t-1]=log_posterior_density(gamma,priorprob,phi,a,b);
+			phi=a/b;
 
-		if(gamma_change(gammaProp,gamma)){
+
+			if(p_gamma!=0){
+				if(gamma_diff) submatrix(xaxa,xaxag,gamma,p,p);
+				dgemv_(&transN , &p, &p_gamma, &unity, &*xaxag.begin(), &na, &*Bg.begin(), &inc, &inputscale0, &*xaya.begin(), &inc);
+				//dtrmv_(&uplo, &transT, &unit_tri, &na, &*xa.begin(), &na, &*xaya.begin(), &inc);
+			}else{
+				for(size_t i=0; i!=xaya.size(); ++i) xaya[i]=0;
+				//dtrmv_(&uplo, &transT, &unit_tri, &na, &*xa.begin(), &na, &*xaya.begin(), &inc);
+			}
+
+			//Compute Probabilities//
+			if(modelprior==1)
+			{
+				bernoulli_probabilities(prob,odds,Bols,d,xoyo,xaya,priorprob,lam,phi);
+			}else if(modelprior==2)
+			{
+				betabinomial_probabilities(prob,odds,Bols,d,xoyo,xaya,theta,lam,phi);
+			}else
+			{
+				uniform_probabilities(prob,odds,Bols,d,xoyo,xaya,lam,phi);
+			}
+
+			for (int i = 0; i < p; ++i)
+			{
+				if(prob[i]*sqrt(phi*(lam[i]+d[i])/(2*M_PI))>(1-prob[i])){
+					gammaProp[i]=1;
+				}else{
+					gammaProp[i]=0;
+				}
+			}
+
+			if(gamma_change(gammaProp,gamma)){
 				if(visitedModels.find(gammaProp)==visitedModels.end()){
 					stackB.push(B);
 					stackgamma.push(gamma);
@@ -173,37 +196,37 @@ extern "C" void em(double * ryo, double * rxo,  double * rlam, int * rmodelprior
 					visitedModels[gammaProp]=true;
 					gamma=gammaProp;
 				}
-				}
-		p_gamma=std::accumulate(gamma.begin(),gamma.end(),0);
-
-
-		for (int i = 0; i < p; ++i)
-		{
-			if(gamma[i]==1){
-				B[i]=(d[i]/(d[i]+lam[i]))*Bols[i];
-			}else{
-				B[i]=0;
 			}
-		}
-		//
-		//Draw Theta//
-//		if(modelprior==2) theta=Rf_rbeta(beta1+p_gamma,p-p_gamma+beta2);
-
-		//Draw Lambda//
-//		if(scalemixture) draw_lambda_t(lam,gamma,alpha,B,phi);
+			p_gamma=std::accumulate(gamma.begin(),gamma.end(),0);
 
 
-		//Store Draws//
-		std::copy(gamma.begin(),gamma.end(),(gamma_trace+p*t));
-		std::copy(prob.begin(),prob.end(),(prob_trace+p*t));
-		std::copy(B.begin(),B.end(),(B_trace+p*t));
-		std::copy(lam.begin(),lam.end(),(lam_trace+p*t));
-		phi_trace[t]=phi;
+			for (int i = 0; i < p; ++i)
+			{
+				if(gamma[i]==1){
+					B[i]=(d[i]/(d[i]+lam[i]))*Bols[i];
+				}else{
+					B[i]=0;
+				}
+			}
+			//
+			//Draw Theta//
+			//		if(modelprior==2) theta=Rf_rbeta(beta1+p_gamma,p-p_gamma+beta2);
 
-		//Has Gamma Changed?//
-		gamma_diff=gamma_change(gamma_trace,t,p);
-		t++;
-	}while (t<niter && (((lpd_trace[t-2]-lpd_trace[t-3])*(lpd_trace[t-2]-lpd_trace[t-3]))>tol));
+			//Draw Lambda//
+			//		if(scalemixture) draw_lambda_t(lam,gamma,alpha,B,phi);
+
+
+			//Store Draws//
+			std::copy(gamma.begin(),gamma.end(),(gamma_trace+p*t));
+			std::copy(prob.begin(),prob.end(),(prob_trace+p*t));
+			std::copy(B.begin(),B.end(),(B_trace+p*t));
+			std::copy(lam.begin(),lam.end(),(lam_trace+p*t));
+			phi_trace[t]=phi;
+
+			//Has Gamma Changed?//
+			gamma_diff=gamma_change(gamma_trace,t,p);
+			t++;
+		}while (t<niter && (((lpd_trace[t-2]-lpd_trace[t-3])*(lpd_trace[t-2]-lpd_trace[t-3]))>tol));
 	} while( t<niter);
 	PutRNGstate();
 	std::cout << "Models Visited: " << visitedModels.size() << std::endl;
